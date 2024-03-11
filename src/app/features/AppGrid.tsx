6333c60d99541c145c2ce8c974617d144097cf3d
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-03-11 11:20:16 +0100

    Appgrid using flex instead of grids. More fluid cards by using flex

--- src/app/features/AppGrid.tsx
+++ src/app/features/AppGrid.tsx
@@ -1,7 +1,8 @@
 import { AppData, AppGridData } from './appGridData';
+
 function AppGrid() {
   return (
-    <div className="app-grid bg-orange-500 h-full w-full">
+    <div className="app-layout bg-orange-500 h-full w-full">
       <AppGridCards />
     </div>
   );
@@ -12,8 +13,8 @@ function AppGridCards() {
     <>
       {AppGridData.map(
         ({ id, image, path, label, title, content }: AppData) => (
-          <article key={id} className="flex">
-            <div className="max-w-[20rem] w-full">
+          <article key={id} className="flex w-auto mt-20 h-auto">
+            <div className="max-w-[18rem] w-full">
               <img
                 className="object-cover aspect-square h-full w-full"
                 src="http://unsplash.it/800/800?random&gravity=center"
@@ -21,7 +22,7 @@ function AppGridCards() {
               />
             </div>
 
-            <div className="bg-pink-500 flex grow flex-col relative">
+            <div className="bg-pink-500 flex grow flex-col relative align-middle">
               <h2>{title}</h2>
               <p>{content}</p>
               <a className="absolute bottom-0" href={path}>

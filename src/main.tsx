04c1c82e9a7cbfbf8b13a51d4c92754c63b1002c
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-03-11 10:34:35 +0100

    Added routes directory. Setting up route to about page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Layout from './layout/Layout.tsx';
 import App from './app/App.tsx';
+import About from './routes/about/About.tsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     element: <Layout />,
-    children: [{ path: '/', element: <App /> }],
+    children: [
+      { path: '/', element: <App /> },
+
+      { path: '/about', element: <About /> },
+    ],
   },
 ]);
 

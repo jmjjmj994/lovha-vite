5dd6805811d778f0625c2f80a51d2c63c1a78712
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-03-14 11:15:39 +0100

    Working with banner, trying plugin for opti

--- src/routes/about/features/Banner.tsx
+++ src/routes/about/features/Banner.tsx
@@ -1,9 +1,9 @@
 import imgAlle from '../../../assets/alle.jpg';
 function AboutBanner() {
   return (
-    <div className='h-[30vh] bg-orange-500'>
-      <img className='object-cover aspect-square h-full w-full' src={imgAlle} alt="Image of the Lovha Studio team members" />
-    </div>
+    <div className='lovha-team h-[50vh]  bg-orange-500'>
+      <img className='object-cover lg:object-cover aspect-square h-full w-full' src={imgAlle} alt="Image of the Lovha Studio team members" />
+     </div>
   );
 }
 export default AboutBanner;

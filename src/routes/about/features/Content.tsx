4cee95f6e6b09587ff7087d880855fc5120d582b
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-03-14 12:27:22 +0100

    Added spacing on text, and a new media query to contain the biggest image on the about page

--- src/routes/about/features/Content.tsx
+++ src/routes/about/features/Content.tsx
@@ -2,7 +2,7 @@ function AboutContent() {
   return (
     <section>
       <h1>Om Oss</h1>
-      <p>
+      <p className="max-w-[75ch]">
         Lovha Studio startet opprinnelig som en liten tanke om å lage et spill
         sammen med klassekamerater, men vi hadde ingen anelse om hva spillet
         skulle handle om. Etter utallige møter og mange diskusjoner over
@@ -12,7 +12,7 @@ function AboutContent() {
         forskjellige ideer, alt fra biler i sandkasser til student apper. Det vi
         til slutt landet på, er 3D-freelance arbeid og vårt spill «Fall Bros».
       </p>
-      <p>
+      <p className="max-w-[75ch] mt-10">
         Vi er fire 3D og spillteknologi entusiaster. Vi er nåværende studenter
         på Fagskolen Noroff i Bergen. Vårt ønske er å gjøre vår interesse om til
         vårt levebrød. Vi elsker alt fra karakter animasjon til statiske mesher
